fee4cd8c58079fcd7e32c1da25a99667af73b3eb
nethmin liyanage <nethmin.liyanage@gmail.com>
2023-03-23 16:30:38 -0700

    tRaiLiNG wHiTeSpAcE or wtv

--- containers/BinaryTree.py
+++ containers/BinaryTree.py
@@ -78,8 +78,7 @@ class BinaryTree():
         elif traversal_type == 'postorder':
             return self.postorder_print(self.root, '')
         else:
-            raise ValueError('Traversal type ' +
-                             str(traversal_type) + ' is not supported.')
+            raise ValueError('travrsltyp' + str(traversal_type) + 'not supp.')
 
     def preorder_print(self, start, traversal):
         '''
